a80690600ebba9cce8e87f4b91830aac4be946c3
hatimaderras <hatem.aderras@gmail.com>
2025-09-24 10:31:23 +0200

    Challenge 2 : Fonction de Multiplication(functions)

---
+++ Challenge 2  Fonction de Multiplication.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void multiplication(int a,int b){
+printf("ur multiplication is :%d",a*b);
+
+}
+
+int main (){
+
+   int c,d;
+
+   printf("enter ur first number:");
+   scanf("%d",&c);
+
+   printf("enter ur secound number:");
+   scanf("%d",&d);
+
+   multiplication(c,d);
+
+
+return 0;
+}
